8e58b4afc33e9a8c78b397cc11189bff39d126c7
Medaboina Mahesh <85110955+maheshmedaboina@users.noreply.github.com>
2024-05-08 10:31:03 +0530

    Create AllDataTypes.java

---
+++ Anudip0705/AllDataTypes.java
@@ -0,0 +1,24 @@
+package Anudip0705;
+// Java Practice -1
+// 1.Write a java program by declaring all data types with proper variable name (following naming conventions) and print the  values.
+
+public class AllDataTypes {
+
+	public static void main(String[] args) {
+		int n = 50;
+		float f = 50.0f;
+		double d = 10.25;
+		String s = "Mahesh";
+		char ch = 'M';
+		boolean isTrue = true;
+		
+		System.out.println("int "+n);
+		System.out.println("float "+f);
+		System.out.println("double "+d);
+		System.out.println("String "+s);
+		System.out.println("char "+ch);
+		System.out.println("boolean "+isTrue);
+
+	}
+
+}
